49a46ea57535d03ec50458179d6356f31752a62d
Mohamud Ali Farah <mohamudalifarah48@gmail.com>
2023-01-25 18:04:14 +0000

    add homepage title images

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,7 +4,7 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
+    title: 'Dev Reference',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
@@ -13,7 +13,7 @@ const FeatureList = [
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Well Orgarnised',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
@@ -22,7 +22,7 @@ const FeatureList = [
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Easy to access',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
